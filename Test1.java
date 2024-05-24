e0f3b9cd707d8cd9e983fe2c1e1675915c69e9e5
shashikant00 <shashikantgit10@gmail.com>
2024-05-24 14:05:51 +0530

    second first

--- Test1.java
+++ Test1.java
@@ -1,7 +1,26 @@
+import java.util.*;
 class Test1
 {
 	public static void main(String[] args)
 	{
 		//WAP to find the Duplicates character in given String?
+		
+		String s="hello";
+		Map<Character,Integer> map=new HashMap<>();
+		
+		char[] chars=s.toCharArray();
+		
+		for(char ch :chars)
+		{
+			if(!map.containsKey(ch))
+			{
+				map.put(ch,1);
+			}else{
+				int count=map.get(ch);
+				map.put(ch,count+1);
+			}
+		}
+		
+		System.out.println(map);
 	}
 }
